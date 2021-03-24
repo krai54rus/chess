937dd26672a5e966eb12b29fff526c6f562a7c01
eushakov@umg-marketing.com <eushakov@umg-marketing.com>
2021-03-24 19:12:07 +0700

    nice commit

--- src/index.js
+++ src/index.js
@@ -1,63 +1,174 @@
-console.log('hello');
-
 let cells = document.querySelectorAll('.cell');
-const chessmenContainer = document.getElementById('chessmen-container'); ;
-let indexY = 0;
+const chessmenContainer = document.getElementById('chessmen-container');
+const tableMeasure = document.getElementById('table').getBoundingClientRect();
 let indexX = 0;
+let indexY = 9;
 let iter = 0;
-const symb = ['a','b','c','d'];
+const symb = ['a','b','c','d','e','f','g','h'];
 const tableCoords = {};
-cells.forEach(function(cell,index){
-    // coords
-    let coords = cell.getBoundingClientRect();
-    console.log(coords);
-
-    indexY++;
-    let ostatok = index % 4;
-    if (!ostatok) {
-        indexX++;
-        indexY = 1;
+let cellsColor = 1;
+const light = '#F2CEA7';
+const brown = '#AE572D';
+function tableColor(cell,index){
+    if (index % 2) {
+        if (cellsColor) {
+            cell.style.backgroundColor = brown;
+        }else{
+            cell.style.backgroundColor = light;
+        }
+        if (index % 8 == 7) {
+            if (cellsColor) {
+                cellsColor = 0;
+            }else{
+                cellsColor = 1;
+            }
+        }
+    }else{
+        if (!cellsColor) {
+            cell.style.backgroundColor = brown;
+        }else{
+            cell.style.backgroundColor = light;
+        }
     }
+}
+cells.forEach(function(cell,index){
+    tableColor(cell,index);
+    // Получаем координаты и размеры клетки
+    let cellMeasure = cell.getBoundingClientRect(),
+    relativePos = {};
+
+    relativePos.x = cellMeasure.left - tableMeasure.left;
+    relativePos.y = cellMeasure.top - tableMeasure.top;
+    // Каждый 4 индекс переносим цифру по X и смещаем Н lj 1
+    indexX++;
+    let ostatok = index % 8;
+    if (!ostatok) {
+        indexY--;
+        indexX = 1;
+    }
+    // Текущие Y и X по шахматной доске
     let currIndexY = indexY;
-    let currIndexX = indexX;
-    // let name = `${symb[currIndexX-1]}${currIndexY}`;
+    let currIndexX = symb[indexX-1];
     let name = `${currIndexX}${currIndexY}`;
+    // Таблица клеток доски с их страничными координатами
     tableCoords[name] = {
-        x:coords.x,
-        y:coords.y
+        x:relativePos.x,
+        y:relativePos.y,
+        width:cellMeasure.width,
+        height:cellMeasure.height
     };
+    // Имя клетки
+    cell.setAttribute('coord',name);
+    // Координаты доски
     cell.setAttribute('data-x',currIndexX);
     cell.setAttribute('data-y',currIndexY);
-    cell.setAttribute('data-pos-x',coords.x);
-    cell.setAttribute('data-pos-y',coords.y);
+    // Координаты страницы
+    cell.setAttribute('data-pos-x',cellMeasure.x);
+    cell.setAttribute('data-pos-y',cellMeasure.y);
 });
 console.log(tableCoords);
-const coords = {
-    x:[1,2,3,4],
-    y: [1,2,3,4]
-}
+// const coords = {
+//     x:[1,2,3,4],
+//     y: [1,2,3,4]
+// }
 
-// const chessmen = 
-
-class Elem{
-    constructor(x,y){
-        this.x = x;
-        this.y = y;
+class Figure {
+    constructor(cell){
+        this.cell = cell;
+        // this.x = x;
+        // this.y = y;
     }
     draw(name){
+        let coordObj = tableCoords[`${this.cell}`];
+
         let elem = document.createElement("div");
         elem.classList.add('chessman');
-        let coordObj = tableCoords[`${this.x}${this.y}`];
-        console.log(coordObj);
-        
-        elem.innerHTML = 'xep';
+
+        elem.innerHTML = `<img class="chessman-img" src="./assets/chessmen/${name}.svg" alt="">`;
+        elem.style.left = `${coordObj.x+(coordObj.width-35)/2}px`;
+        elem.style.top =`${coordObj.y+(coordObj.width-35)/2}px`;
         chessmenContainer.append(elem);
-        //elem.setAttribute("style", `left:${coordObj.x}; top: ${coordObj.y};`);
-        elem.style.left = `${coordObj.x}px`;
-        elem.style.top =`${coordObj.y}px`;
-        
-        // return name;
+    }
+    move(){
+        console.log('move');
     }
 }
 
-new Elem(1,2).draw('pawn');
+class Table {
+    constructor(){
+        this.whitePos = [
+            {
+                cell:'a2',
+                name:'pawn'
+            },
+            {
+                cell:'b2',
+                name:'pawn'
+            },
+            {
+                cell:'c2',
+                name:'pawn'
+            },
+            {
+                cell:'d2',
+                name:'pawn'
+            },
+            {
+                cell:'e2',
+                name:'pawn'
+            },
+            {
+                cell:'f2',
+                name:'pawn'
+            },
+            {
+                cell:'g2',
+                name:'pawn'
+            },
+            {
+                cell:'h2',
+                name:'pawn'
+            },
+            {
+                cell:'a1',
+                name:'rook'
+            },
+            {
+                cell:'b1',
+                name:'knight'
+            },
+            {
+                cell:'c1',
+                name:'bishop'
+            },
+            {
+                cell:'d1',
+                name:'queen'
+            },
+            {
+                cell:'e1',
+                name:'king'
+            },
+            {
+                cell:'f1',
+                name:'bishop'
+            },
+            {
+                cell:'g1',
+                name:'knight'
+            },
+            {
+                cell:'h1',
+                name:'rook'
+            }
+        ]
+    }
+    init(){
+        this.whitePos.forEach(function(figure){
+            new Figure(figure.cell).draw(figure.name);
+        });
+    }
+}
+new Table().init();
+
+// new Figure('a2').draw('pawn');
